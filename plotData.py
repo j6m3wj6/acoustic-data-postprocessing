4cf92e639f0c11775024a383fe9f4b33aa73e310
Tong Wang (王予彤) <Tong.Wang@tymphany.com>
2021-04-12 17:53:04 +0800

    Qwidget in QMainWindow

--- plotData.py
+++ plotData.py
@@ -1,129 +1,160 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import (QApplication, QWidget, QFileDialog, QTextEdit, QPushButton, QLabel, QVBoxLayout, QTreeWidget, QTreeWidgetItem)
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QFileDialog, QTextEdit, 
+  QPushButton, QLabel, QVBoxLayout, QTreeWidget, QTreeWidgetItem, QMenuBar, QMenu)
 from pyqtgraph import PlotWidget, plot
-import pyqtgraph as pg
-import sys  # We need sys so that we can pass argv to QApplication
-import os
-import numpy as np
-import pandas as pd
-
+import pyqtgraph as pg, numpy as np, pandas as pd
+import sys, os
 import matplotlib
 matplotlib.use('Qt5Agg')
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
+from matplotlib import pyplot as plt
+import mplcursors
 
 class MplCanvas(FigureCanvasQTAgg):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
+    def __init__(self, parent=None):
+        fig = Figure()
         # fig.canvas.mpl_connect('button_press_event', onclick)
         self.axes = fig.add_subplot(111)
+        self.axes.set_xscale('log')
+        self.axes.set_xlim([20,20000])
+        self.axes.set_ylim(auto=True)
+        self.axes.grid()
+        self.axes.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
         super(MplCanvas, self).__init__(fig)
 
 class PlotGraph(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.resize(800, 600)
-        self.button1 = QPushButton('Upload data')
-        self.button1.clicked.connect(self.plotData)
-        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
-        toolbar = NavigationToolbar(self.sc, self)
+  """Widget for visualize data"""
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.initUI()
+# User Interface
+  def initUI(self):
+    self._createButton()
+    self._createCanvas()
+    self._createTreeList()
 
-        self.tree=QTreeWidget()
-        #设置列数
-        self.tree.setColumnCount(2)
-        #设置树形控件头部的标题
-        self.tree.setHeaderLabels(['Key','Value'])
-        #设置树形控件的列的宽度
-        self.tree.setColumnWidth(0,150)
-        #加载根节点的所有属性与子控件
-        self.tree.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        
-        self.tree.itemClicked.connect(self.printItemText)
+    layout = QVBoxLayout()
+    layout.addWidget(self.button1)
+    layout.addWidget(self.toolbar)
+    layout.addWidget(self.canvas)
+    layout.addWidget(self.tree)
+    self.setLayout(layout)
+  
+  def _createButton(self):
+    self.button1 = QPushButton('Upload data')
+    self.button1.clicked.connect(self.plotData)
 
-        #节点全部展开
-        self.tree.expandAll()
+  def _createCanvas(self):
+    self.canvas = MplCanvas(self)
+    self.toolbar = NavigationToolbar(self.canvas, self)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.button1)
-        layout.addWidget(toolbar)
-        layout.addWidget(self.sc)
-        layout.addWidget(self.tree)
+  def _createTreeList(self):
+    self.tree=QTreeWidget()
+    self.tree.setColumnCount(2)
+    self.tree.setHeaderLabels(['Key','Value'])
+    self.tree.setColumnWidth(0,150)
+    self.tree.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+    self.tree.itemClicked.connect(self.printItemText)
+    self.tree.expandAll()
 
-        self.setLayout(layout)
+# Functions
+  def printItemText(self):
+    items = self.tree.selectedItems()
+    x = []
+    for i in range(len(items)):
+        x.append(str(self.tree.selectedItems()[i].parent().text(0) + self.tree.selectedItems()[i].text(0)))
+    print (x)
 
-    def printItemText(self):
-      items = self.tree.selectedItems()
-      x = []
-      for i in range(len(items)):
-          x.append(str(self.tree.selectedItems()[i].parent().text(0) + self.tree.selectedItems()[i].text(0)))
-      print (x)
-      self.refreshData([1,2,3])
+  def appendChildonTree(self):
+    root=QTreeWidgetItem(self.tree)
+    root.setText(0,'Curve')
+    for i in range(len(self.data.columns)):
+      child=QTreeWidgetItem()
+      child.setText(0,self.data.columns[i])
+      root.addChild(child)
+    self.tree.addTopLevelItem(root)
 
-    def appendChildonTree(self):
-      root=QTreeWidgetItem(self.tree)
-      root.setText(0,'Curve')
-      for i in range(len(self.data.columns)):
-        child=QTreeWidgetItem()
-        child.setText(0,self.data.columns[i])
-        root.addChild(child)
-      self.tree.addTopLevelItem(root)
+  def get_text_file(self):
+    dialog = QFileDialog()
+    dialog.setFileMode(QFileDialog.AnyFile)
+    dialog.setFilter(QtCore.QDir.Files)
 
-    def plot(self, x, y, plotname, color):
-      pen = pg.mkPen(color=color)
-      self.graphWidget.plot(x, y, name=plotname, pen=pen, symbol='+', symbolSize=30, symbolBrush=(color))
+    if dialog.exec_():
+      file_name = dialog.selectedFiles()
 
-    def get_text_file(self):
-      dialog = QFileDialog()
-      dialog.setFileMode(QFileDialog.AnyFile)
-      dialog.setFilter(QtCore.QDir.Files)
+      if file_name[0].endswith('.txt'):
+        with open(file_name[0], 'r', encoding='UTF-8') as file:
+          
+          headers = file.readlines()[:3]
+          print(headers[0])
+          title = headers[0]
+          curves = headers[1].split('\t\t')
+          curves = [c.replace('"', '').strip() for c in curves]
+          
+          dtbl = pd.read_table(file_name[0],  skiprows=2)
+          freq = dtbl.iloc[:, 0]
+          freq = [float(f.replace(',', '').strip()) for f in freq]
+          dtbl = dtbl.iloc[:, [i%2==1 for i in range(len(dtbl.columns))]]
+          dtbl.columns = curves
+          
+          dtbl['Frequency'] = freq
+          file.close()
+          return dtbl
+      else:
+        pass
 
-      if dialog.exec_():
-        file_name = dialog.selectedFiles()
+  def plotData(self):
+    self.data = self.get_text_file()
+    print(self.data)
+    lines = []
+    for col in self.data.columns[:-1]:
+        line, = self.canvas.axes.plot(self.data["Frequency"], self.data[col], label=col)
+        lines.append(line)
+    mplcursors.cursor(lines, highlight=True, highlight_kwargs=dict(linewidth=5))
+    self.canvas.axes.legend(loc='lower left')
+    # self.data.plot("Frequency", ax = self.canvas.axes)
+    self.canvas.draw()
+    self.appendChildonTree()
 
-        if file_name[0].endswith('.txt'):
-          with open(file_name[0], 'r', encoding='UTF-8') as file:
-            
-            headers = file.readlines()[:3]
-            print(headers[0])
-            title = headers[0]
-            curves = headers[1].split('\t\t')
-            curves = [c.replace('"', '').strip() for c in curves]
-            
-            dtbl = pd.read_table(file_name[0],  skiprows=2)
-            freq = dtbl.iloc[:, 0]
-            freq = [float(f.replace(',', '').strip()) for f in freq]
-            dtbl = dtbl.iloc[:, [i%2==1 for i in range(len(dtbl.columns))]]
-            dtbl.columns = curves
-            
-            dtbl['Frequency'] = freq
-            file.close()
-            return dtbl
-        else:
-          pass
+  def onclick(event):
+        print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+              ('double' if event.dblclick else 'single', event.button,
+              event.x, event.y, event.xdata, event.ydata))
 
-    def plotData(self):
-      self.data = self.get_text_file()
-      print(self.data)
-      self.data.plot("Frequency", ax = self.sc.axes)
-      self.sc.draw()
-      self.appendChildonTree()
+class MyApp(QMainWindow):
+  """App's Main Window."""
+  def __init__(self, parent=None):
+    """Initializer."""
+    super().__init__(parent)
+    self.initUI()
 
-    def refreshData(self, selectedData):
-      self.data.plot("Frequency", ax = self.sc.axes, y=selectedData)
-      print(selectedData)
+  def initUI(self):  
+    self.setWindowTitle("Python Menus & Toolbars")
+    self.resize(800, 600)
+    self.setCentralWidget(PlotGraph())
+    self._createMenuBar()
 
-      self.sc.draw()
 
-def onclick(event):
-    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
-          ('double' if event.dblclick else 'single', event.button,
-           event.x, event.y, event.xdata, event.ydata))
-    
+  def _createMenuBar(self):
+    menuBar = QMenuBar(self)
+    # Creating menus using a QMenu object
+    fileMenu = QMenu("&File", self)
+    menuBar.addMenu(fileMenu)
+    # Creating menus using a title
+    editMenu = menuBar.addMenu("&Edit")
+    helpMenu = menuBar.addMenu("&Help")
+    self.setMenuBar(menuBar)
+
+
+
 def main():
-    app = QApplication(sys.argv)
-    main = PlotGraph()
-    main.show()
-    sys.exit(app.exec_())
+  app = QApplication(sys.argv)
+  #main = PlotGraph()
+  main = MyApp()
+  main.show()
+  sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
