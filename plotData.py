9b4d3f05e22d1f2652dac5b1a86e05c16e2c41f8
Tong Wang (王予彤) <Tong.Wang@tymphany.com>
2021-04-22 18:02:49 +0800

    LEAP file

--- plotData.py
+++ plotData.py
@@ -4,7 +4,7 @@ import numpy as np, pandas as pd
 import sys, os
 import matplotlib
 matplotlib.use('Qt5Agg')
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseButton
@@ -15,13 +15,11 @@ _highlightLineWidth = 4
 class MplCanvas(FigureCanvasQTAgg):
   def __init__(self, parent=None):
     # Canvas init
-    self.fig = Figure() # figsize=(6,6)
-    # fig.canvas.mpl_connect('button_press_event', self.onclick)
+    self.fig = Figure(figsize=(12,4)) # figsize=(6,6)
     self.ax = self.fig.add_subplot(111)
     self._setStyle()
     super(MplCanvas, self).__init__(self.fig)
 
-    # snap_cursor = SnappingCursor(ax, line)
     self.horizontal_line = self.ax.axhline(color='k', lw=0.8, ls='--')
     self.vertical_line = self.ax.axvline(color='k', lw=0.8, ls='--')
     self.text = self.ax.text(0.72, 0.9, '', transform=self.ax.transAxes)
@@ -29,12 +27,12 @@ class MplCanvas(FigureCanvasQTAgg):
   def _setStyle(self):
     # axes' style
     self.ax.set_xscale('log')
-    self.ax.set_xlim([20,20000])
-    self.ax.set_ylim([20,100])s
+    self.ax.set_xlim(auto=True)
+    self.ax.set_ylim(auto=True)
     self.ax.grid()
     self.ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
     self.fig.tight_layout()
-    self.fig.subplots_adjust(right=0.75)
+    self.fig.subplots_adjust(right=0.8)
 
   def set_cross_hair_visible(self, visible):
     need_redraw = self.horizontal_line.get_visible() != visible
@@ -43,19 +41,35 @@ class MplCanvas(FigureCanvasQTAgg):
     self.text.set_visible(visible)
     return need_redraw
 
- 
+class MyToolBar(NavigationToolbar2QT):
+    def __init__(self,canvas_,parent_):
+      self.toolitems = (
+          ('Home', 'Lorem ipsum dolor sit amet', 'home', 'home'),
+          ('Back', 'consectetuer adipiscing elit', 'back', 'back'),
+          ('Forward', 'sed diam nonummy nibh euismod', 'forward', 'forward'),
+          (None, None, None, None),
+          ('Pan', 'tincidunt ut laoreet', 'move', 'pan'),
+          ('Zoom', 'dolore magna aliquam', 'zoom_to_rect', 'zoom'),
+          (None, None, None, None),
+          ('Subplots', 'putamus parum claram', 'subplots', 'configure_subplots'),
+          ('Save', 'sollemnes in futurum', 'filesave', 'save_figure'),
+          ('Port', 'Select', "select", 'select_tool'),
+          )
+      NavigationToolbar2QT.__init__(self,canvas_,parent_)
+    def select_tool(self):
+      print("You clicked the selection tool")
 
 class PlotGraph(QWidget):
   """Widget for visualize data"""
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.initUI()
-    self.ydata = pd.DataFrame()
+    self.data = pd.DataFrame()
 # User Interface
   def initUI(self):
     # create components
     self._createCanvas()
-    self.canvas.fig.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
+    # self.canvas.fig.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
 
     self._createTreeList()
     self._createButton()
@@ -73,7 +87,8 @@ class PlotGraph(QWidget):
     # 3
     hboxLayout_btn = QHBoxLayout()
     vboxlayout_data = QVBoxLayout()
-    vboxlayout_data.addWidget(self.btn_importData)
+    vboxlayout_data.addWidget(self.btn_importKlippelData)
+    vboxlayout_data.addWidget(self.btn_importLEAPData)
     vboxlayout_data.addWidget(self.btn_clearData)
     hboxLayout_btn.addLayout(vboxlayout_data)
     # 4
@@ -85,7 +100,7 @@ class PlotGraph(QWidget):
 
   def _createCanvas(self):
     self.canvas = MplCanvas(self)
-    self.toolbar = NavigationToolbar(self.canvas, self)
+    self.toolbar = MyToolBar(self.canvas, self)
     self.canvas.fig.canvas.mpl_connect('button_press_event', self.canvas_handleClick)
 
   def _createShiftGridGroupBox(self):
@@ -104,8 +119,10 @@ class PlotGraph(QWidget):
     self.shiftGridGroupBox.setLayout(layout)
 
   def _createButton(self):
-    self.btn_importData = QPushButton('Import data')
-    self.btn_importData.clicked.connect(self.plotData)
+    self.btn_importKlippelData = QPushButton('Import Klippel data')
+    self.btn_importLEAPData = QPushButton('Import LEAP data')
+    self.btn_importKlippelData.clicked.connect(self.plotKlippelData)
+    self.btn_importLEAPData.clicked.connect(self.plotLEAPData)
     self.btn_clearData = QPushButton('Clear data')
     self.btn_clearData.clicked.connect(self.clearData)
 
@@ -173,12 +190,13 @@ class PlotGraph(QWidget):
     print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
         ('double' if event.dblclick else 'single', event.button,
         event.x, event.y, event.xdata, event.ydata))
-    if self.ydata.empty or not event.inaxes: return
+    if self.data.empty or not event.inaxes: return
 
     line_i = self._findSelectedLine(event.xdata, event.ydata)
     if (line_i == -1):
       self._resetLineWidth()
-    elif (event.button == MouseButton.RIGHT and self.canvas.ax.lines[line_i].get_linewidth() == 4):
+    elif (event.button == MouseButton.RIGHT and\
+          self.canvas.ax.lines[line_i].get_linewidth() == 4):
       self.canvas.ax.lines[line_i].set_linewidth(_defaultLineWidth)
     else:
       self.canvas.ax.lines[line_i].set_linewidth(_highlightLineWidth)
@@ -186,22 +204,22 @@ class PlotGraph(QWidget):
 
   def _findSelectedLine(self, cursor_x, cursor_y):
     min_i = 0
-    max_i = len(self.xdata_freq)    
+    max_i = len(self.data['Frequency'])    
     while (max_i - min_i > 1):
       middle_i = int((max_i - min_i)/2) + min_i
-      if (cursor_x > float(self.xdata_freq[middle_i])):
+      if (cursor_x > float(self.data['Frequency'][middle_i])):
         min_i = middle_i
       else:
         max_i = middle_i
-    if (cursor_x - self.xdata_freq[min_i] > self.xdata_freq[max_i] - cursor_x):
+    if (cursor_x - self.data['Frequency'][min_i] > self.data['Frequency'][max_i] - cursor_x):
       freq_i = max_i
     else:
       freq_i = min_i
     error = float('inf')
-    for i in range(len(self.ydata.iloc[freq_i])):
-      # print(abs(cursor_y - float(self.ydata.iloc[freq_i][i])), error)
-      if (abs(cursor_y - float(self.ydata.iloc[freq_i][i])) < error):
-        error = abs(cursor_y - float(self.ydata.iloc[freq_i][i]))
+    for i in range(len(self.data.iloc[freq_i])):
+      # print(abs(cursor_y - float(self.data.iloc[freq_i][i])), error)
+      if (abs(cursor_y - float(self.data.iloc[freq_i][i])) < error):
+        error = abs(cursor_y - float(self.data.iloc[freq_i][i]))
         line_i = i
     # print(line_i)
     if (error > 1): return -1
@@ -209,24 +227,24 @@ class PlotGraph(QWidget):
   
   def _findYdata(self, cursor_x, cursor_y, line_index):
     left_x = 0
-    right_x = len(self.xdata_freq)
+    right_x = len(self.data['Frequency'])
     while (right_x - left_x > 1):
       middle_freq = int((right_x - left_x)/2) + left_x
-      if (cursor_x > float(self.xdata_freq[middle_freq])):
+      if (cursor_x > float(self.data['Frequency'][middle_freq])):
         left_x = middle_freq
       else:
         right_x = middle_freq
     
-    left_freq = self.xdata_freq[left_x]
-    right_freq = self.xdata_freq[right_x]
-    left_ydata = self.ydata.iloc[left_x][line_index]
-    right_ydata = self.ydata.iloc[right_x][line_index]
+    left_freq = self.data['Frequency'][left_x]
+    right_freq = self.data['Frequency'][right_x]
+    left_ydata = self.data.iloc[left_x][line_index]
+    right_ydata = self.data.iloc[right_x][line_index]
 
     estimate_ydata = left_ydata + ((cursor_x - left_freq) / (right_freq - left_freq))*(right_ydata - left_ydata)
     return estimate_ydata
 
   def on_mouse_move(self, event):
-    if self.ydata.empty or not event.inaxes: return
+    if self.data.empty or not event.inaxes: return
     else:
       self.canvas.set_cross_hair_visible(True)
       x = event.xdata
@@ -241,7 +259,37 @@ class PlotGraph(QWidget):
     print(self.cbox_shift.currentText())
     
 # Import data - data preprocessing
-  def _get_text_file(self):
+  def _get_LEAP_text_file(self):
+    dialog = QFileDialog()
+    dialog.setFileMode(QFileDialog.AnyFile)
+    dialog.setFilter(QtCore.QDir.Files)
+
+    if dialog.exec_():
+      file_name = dialog.selectedFiles()
+
+      if file_name[0].endswith('.txt'):
+        with open(file_name[0], 'r', encoding='UTF-8', errors='ignore') as file:
+          file_dir = file_name[0]
+          self.filename = file_name[0][file_dir.rfind('/')+1:file_dir.rfind('.')]         
+          
+          print(file_dir)
+          headers = file.readlines()[:11]
+          self.title = headers[0]
+
+          cols = headers[-1]
+          cols = cols.strip().split(" ")
+          cols = [x for x in cols if x][2:]
+          cols[0] = 'Frequency'
+          data = pd.read_csv(file_name[0],  skiprows=11)
+          data.columns = cols
+
+          
+          file.close()
+          return data
+      else:
+        pass
+
+  def _get_Klippel_text_file(self):
     dialog = QFileDialog()
     dialog.setFileMode(QFileDialog.AnyFile)
     dialog.setFilter(QtCore.QDir.Files)
@@ -251,21 +299,22 @@ class PlotGraph(QWidget):
 
       if file_name[0].endswith('.txt'):
         with open(file_name[0], 'r', encoding='UTF-8') as file:
+          file_dir = file_name[0]
+          self.filename = file_name[0][file_dir.rfind('/')+1:file_dir.rfind('.')]         
           
           headers = file.readlines()[:3]
-          # print(headers[0])
-          file_dir = file_name[0]
-          self.filename = file_name[0][file_dir.rfind('/')+1:file_dir.rfind('.')]
           self.title = headers[0]
           curves = headers[1].split('\t\t')
           curves = [c.replace('"', '').strip() for c in curves]
           
           data = pd.read_table(file_name[0],  skiprows=2)
+          data = data.dropna() 
           freq = data.iloc[:, 0]
-          self.xdata_freq = [float(f.replace(',', '').strip()) for f in freq]
+          freq = [float(f.replace(',', '').strip()) for f in freq]
           data = data.iloc[:, [i%2==1 for i in range(len(data.columns))]]
           data.columns = curves
           
+          data.insert(0, 'Frequency', freq)
           file.close()
           return data
       else:
@@ -281,7 +330,7 @@ class PlotGraph(QWidget):
       
       data = pd.read_table('bose.txt',  skiprows=2)
       freq = data.iloc[:, 0]
-      self.xdata_freq = [float(f.replace(',', '').strip()) for f in freq]
+      self.data['Frequency'] = [float(f.replace(',', '').strip()) for f in freq]
       data = data.iloc[:, [i%2==1 for i in range(len(data.columns))]]
       data.columns = curves
       
@@ -293,24 +342,37 @@ class PlotGraph(QWidget):
     root=QTreeWidgetItem(self.tree)
     root.setText(0, self.filename)
     root.setCheckState(0, 2)
-    for i in range(len(self.ydata.columns)):
+    for col in self.data.columns[1:]:
       child = QTreeWidgetItem()
-      child.setText(0,self.ydata.columns[i])
+      child.setText(0, col)
       child.setCheckState(0, 2)
       root.addChild(child)
     self.tree.addTopLevelItem(root)
     self.tree.expandAll()
 
-    self.cbox_shift.addItems(self.ydata.columns)
+    self.cbox_shift.addItems(self.data.columns[1:])
 
-  def plotData(self):
-    # self.ydata = self._get_text_file()
-    self.ydata = self._get_bose()
-    # print(self.ydata)
-    # print(self.xdata_freq)
+  def plotKlippelData(self):
+    self.data = self._get_Klippel_text_file()
+    # self.data = self._get_bose()
+    # print(self.data)
+    # print(self.data['Frequency'])
 
-    for col in self.ydata.columns:
-      line, = self.canvas.ax.plot(self.xdata_freq, self.ydata[col], label=col) # marker='o'
+    for col in self.data.columns[1:]:
+      line, = self.canvas.ax.plot(self.data['Frequency'], self.data[col], label=col) # marker='o'
+
+    self.canvas.ax.legend(bbox_to_anchor=(1.04,1), loc="upper left")
+    self.canvas.draw()
+    self.appendChildonTree()
+
+  def plotLEAPData(self):
+    self.data = self._get_LEAP_text_file()
+    # self.data = self._get_bose()
+    print(self.data)
+    # print(self.data['Frequency'])
+
+    for col in self.data.columns[1:]:
+      self.canvas.ax.plot(self.data['Frequency'], self.data[col], label=col) # marker='o'
 
     self.canvas.ax.legend(bbox_to_anchor=(1.04,1), loc="upper left")
     self.canvas.draw()
@@ -333,11 +395,11 @@ class PlotGraph(QWidget):
     except ValueError:
       print('ERROR: can not turn ' + self.le_offsetInput.text())
       return
-    new_data = [d+offset for d in self.ydata[self.cbox_shift.currentText()]]
+    new_data = [d+offset for d in self.data[self.cbox_shift.currentText()]]
     ## Also change origin data
-    # self.ydata[self.cbox_shift.currentText()] = new_data
+    # self.data[self.cbox_shift.currentText()] = new_data
 
-    self.canvas.ax.lines[self.cbox_shift.currentIndex()].set_data(self.xdata_freq, new_data)
+    self.canvas.ax.lines[self.cbox_shift.currentIndex()].set_data(self.data['Frequency'], new_data)
     self.canvas.draw()
 
   def dialog_shift(self):
@@ -370,7 +432,7 @@ class MyApp(QMainWindow):
 
   def initUI(self):  
     self.setWindowTitle("Python Menus & Toolbars")
-    # self.resize(1400, 600)
+    self.resize(1600, 800)
     self.setCentralWidget(PlotGraph())
     self._createMenuBar()
 
