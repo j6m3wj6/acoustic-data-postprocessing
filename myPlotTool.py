3ce7f84a8b0433ed38712613eccc334bd1c4d2fe
Tong Wang (王予彤) <Tong.Wang@tymphany.com>
2021-04-28 17:13:05 +0800

    import multiple files

--- myPlotTool.py
+++ myPlotTool.py
@@ -27,6 +27,7 @@ class CurveData:
 		self.ydata = ydata
 		self.type = _type
 		self.units = units
+		self.line = None
 
 
 
@@ -60,16 +61,20 @@ class MplCanvas(FigureCanvasQTAgg):
 			ax.set_xscale('log')
 			ax.set_xlim([20,20000])
 			ax.set_ylim(auto=True)
-			ax.patch.set_alpha(0.0)
 			ax.grid()
 			ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
-	
+			ax.patch.set_alpha(0.0)
+
 	
 	def set_AxType(self, axIndex, _type):
 		self.axType[axIndex] = _type
 	
 	def get_AxType(self, axIndex):
 		return self.axType[axIndex]
+	
+	def _replot(self):
+		self.fig.axes[0].legend(bbox_to_anchor=(1.04,1), loc="upper left")
+		self.draw()
 
 class PlotGraph(QWidget):
 	def __init__(self, *args, **kwargs):
@@ -91,8 +96,8 @@ class PlotGraph(QWidget):
 		# 2
 		grid_layout = QGridLayout()
 
-		self.tree.setMinimumWidth(400)
-		self.tree.setColumnWidth(0,200)
+		self.tree.setMinimumWidth(600)
+		self.tree.setColumnWidth(0,300)
 		self.canvas.setMinimumWidth(1000)
 
 		grid_layout.addWidget(self.canvas, 0, 0, 1, 1)
@@ -122,7 +127,7 @@ class PlotGraph(QWidget):
 		self.btn_importNFSData = QPushButton('Import NFS data')
 		self.btn_importAPData.clicked.connect(self.plotAPData)
 		self.btn_importLEAPData.clicked.connect(self.plotLEAPData)
-		self.btn_importNFSData.clicked.connect(self.plotNFSData)
+		self.btn_importNFSData.clicked.connect(self.plotKlippelData)
 		self.btn_clearData = QPushButton('Clear data')
 		self.btn_clearData.clicked.connect(self.clearData)
 
@@ -146,12 +151,12 @@ class PlotGraph(QWidget):
 				with open(path, 'r', encoding='UTF-8', errors='ignore') as file:
 					dataSequence = {}
 					headers = file.readlines()[:11]
-					title = path[path.rfind('/')+1:path.rfind('.')] # LEAP_Impedance
+					title = path[path.rfind('/')+1:path.rfind('.')].strip() # LEAP_Impedance
 					
 					if title in dataSequence: pass
 					else: dataSequence[title] = []
 							
-					label = headers[4][headers[4].find('=')+1:]   # Impedance_PR: T201100003660
+					label = headers[4][headers[4].find('=')+1:].strip()   # Impedance_PR: T201100003660
 
 					units = headers[-1]
 					units = units.strip().split(" ")
@@ -165,13 +170,13 @@ class PlotGraph(QWidget):
 					curveDatas = []
 					note = ""
 
-					if ("SPL" in title):
-						curveDatas.append(CurveData(label=label, xdata=freq, ydata=val, _type = CurveType.FreqRes))
-					elif("Impedance" in title):
-						curveDatas.append(CurveData(label=label, xdata=freq, ydata=val, _type = CurveType.IMP))
+					_type = CurveType.NoType
+					if ("Impedance" in title): _type = CurveType.IMP
+					elif ("SPL" in title): _type = CurveType.FreqRes
 
+					curveDatas.append(CurveData(label=label, xdata=freq, ydata=val, _type = _type))
 					curveDatas.append(CurveData(label=label, xdata=freq, ydata=phase, _type = CurveType.Phase))
-					
+
 					dataSequence[title].extend(curveDatas)
 					file.close()
 					return path, dataSequence
@@ -190,21 +195,20 @@ class PlotGraph(QWidget):
 				dataSequence = {}
 				data = pd.read_excel(path, engine="openpyxl", sheet_name=None)
 				# del data['Summary']
-
 				for key, value in data.items():
-					title = data[key].columns[0]
+					title = data[key].columns[0].strip()
 					
 
 					_type = CurveType.NoType
 					if ("Phase" in title): _type = CurveType.Phase 
 					elif ("Impedance" in title): _type = CurveType.IMP
+					elif ("RMS" in title): _type = CurveType.FreqRes
 
-					note = data[key].columns[1] 
+					note = data[key].columns[1].strip()
 					curveDatas = []
 					isline = True
 					for curveIndex in range(int(len(data[key].columns)/2)):
-						
-						label = data[key].iloc[0, curveIndex*2]
+						label = data[key].iloc[0, curveIndex*2].strip()
 						curve_x = pd.Series(data[key].iloc[3:, curveIndex*2], name='x', dtype=float)
 						curve_y = pd.Series(data[key].iloc[3:, curveIndex*2+1], name='y', dtype=float)
 						
@@ -221,60 +225,87 @@ class PlotGraph(QWidget):
 			else:
 				pass
 
+	def _load_Klippel_file(self):
+		dialog = QFileDialog()
+		dialog.setFileMode(QFileDialog.AnyFile)
+		dialog.setFilter(QtCore.QDir.Files)
+
+		if dialog.exec_():
+			file_name = dialog.selectedFiles()
+			path = file_name[0]
+			if path.endswith('.txt'):
+				with open(path, 'r', encoding='UTF-8') as file:
+					file_dir = path
+					filename = path[file_dir.rfind('/')+1:file_dir.rfind('.')]         
+
+					headers = file.readlines()[:3]
+					title = headers[0].strip().strip('"')
+					labels = headers[1].split('\t\t')
+					labels = [c.replace('"', '').strip() for c in labels]
+					dataSequence = {}	
+					data = pd.read_table(path,  skiprows=2)
+					data = data.dropna() 
+					curveDatas = []
+					note = ""
+					
+					freq = data.iloc[:, 0]
+					freq = [float(f.replace(',', '').strip()) for f in freq]
+					freq = pd.Series(freq, name='y', dtype=float)
+					
+					if ("Phase" in title): _type = CurveType.Phase 
+					elif ("Impedance" in title): _type = CurveType.IMP
+					elif ("SPL" in title or "CEA" in title): _type = CurveType.FreqRes
+
+
+					for i in range(int(len(data.columns)/2)):
+						spl = pd.Series(data.iloc[:, i*2+1], name='y', dtype=float)
+						curveDatas.append(CurveData(label=labels[i], note=note, xdata=freq, ydata=spl, _type = _type))
+
+					if title in dataSequence: pass
+					else: dataSequence[title] = []
+					dataSequence[title].extend(curveDatas)
+
+				return path, dataSequence
+			else:
+				pass
+
 	def plotAPData(self):
 		filename, dataSequence = self._load_AP_file()
-		# print(dataSequence)
-		for title, cruvesArr in dataSequence.items():
-			for it in cruvesArr:
-					current_ax = self.canvas.fig.axes[0]
-					if (self._check_and_set_AxType(0, it.type)): pass
-					else:	
-						if (self._check_and_set_AxType(1, it.type)):
-							current_ax = self.canvas.fig.axes[1]
-							self.canvas.fig.axes[1].set_visible(True)
-						else: 
-							print("no ax can plot")
-							return
-					print(it.type)
-					line, = current_ax.plot(it.xdata, it.ydata, label=it.label)
-					it.line = line
-
-		self.fileDict[filename] = dataSequence
-		self.canvas.setAxesStyle()
-		self.canvas.draw()
-		self.appendChildrenTree(filename, dataSequence)
-
+		self.plotData(filename, dataSequence)		
+	def plotLEAPData(self):
+		filename, dataSequence = self._load_LEAP_file()
+		self.plotData(filename, dataSequence)
+	def plotKlippelData(self):
+		filename, dataSequence = self._load_Klippel_file()
+		self.plotData(filename, dataSequence)
+	
 	def _check_and_set_AxType(self, axIndex, _type):
 		if (self.canvas.get_AxType(axIndex) is CurveType.NoType or self.canvas.get_AxType(axIndex) is _type):
 			self.canvas.set_AxType(axIndex, _type)
 			return True
 		else: return False
 
-	def plotLEAPData(self):
-		filename, dataSequence = self._load_LEAP_file()
-
+	def plotData(self, filename, dataSequence):
 		for title, cruvesArr in dataSequence.items():
 			for it in cruvesArr:
-					current_ax = self.canvas.fig.axes[0]
-					if (self._check_and_set_AxType(0, it.type)): pass
-					else:	
-						if (self._check_and_set_AxType(1, it.type)):
-							self.canvas.fig.axes[0].twinx()
-							current_ax = self.canvas.fig.axes[1]
-						else: 
-							print("no ax can plot")
-							return
-					print(it.type)
-					line, = current_ax.plot(it.xdata, it.ydata, label=it.label)
-					it.line = line
+				print(it.type, self.canvas.get_AxType(0), self.canvas.get_AxType(1))
+				current_ax = self.canvas.fig.axes[0]
+				if (self._check_and_set_AxType(0, it.type)): pass
+				else:	
+					if (self._check_and_set_AxType(1, it.type)):
+						current_ax = self.canvas.fig.axes[1]
+						self.canvas.fig.axes[1].set_visible(True)
+					else: 
+						print("no ax can plot")
+						continue
+				line, = current_ax.plot(it.xdata, it.ydata, label=it.label)
+				it.line = line
+
 		self.fileDict[filename] = dataSequence
 		self.canvas.setAxesStyle()
-		self.canvas.draw()
-		# print(dataSequence)
+		self.canvas._replot()
 		self.appendChildrenTree(filename, dataSequence)
 		
-	def plotNFSData(self):
-		print('plotNFSData')
 
 	def get_ax(line):
 		print(line.type)
@@ -282,7 +313,6 @@ class PlotGraph(QWidget):
 	def appendChildrenTree(self, filename, dataSequence):
 		fileroot = QTreeWidgetItem(self.tree)
 		fileroot.setText(0, filename)
-
 		for title, lines in dataSequence.items():
 			root = QTreeWidgetItem()
 			root.setText(0, title)
@@ -300,24 +330,36 @@ class PlotGraph(QWidget):
 
 # Handle Function
 	def tree_handleCheck(self, item): 
-		if not item.parent(): 
+		if not item.parent(): # file root
 			pass
 			# title = item.text(0)
-			# for index in range(item.childCount()):
-			#   child = item.child(index)
-			#   if (item.checkState(0) == 0): child.setCheckState(0, QtCore.Qt.Unchecked)
-			#   else: child.setCheckState(0, QtCore.Qt.Checked)
-			#   self._switchCheckStatusWithFigure(title, index, child.checkState(0), child.text(0))
+		elif(not item.data(0, QtCore.Qt.UserRole)):
+			print("test root")
+			for index in range(item.childCount()):
+				child = item.child(index)
+				child.setCheckState(0, item.checkState(0))
+				curve = child.data(0, QtCore.Qt.UserRole)
+				if (curve.line):
+					if (item.checkState(0) == 0): 
+						curve.line.set_visible(False)
+						curve.line.set_label('_nolegend_')
+					else: 
+						curve.line.set_visible(True)
+						curve.line.set_label(curve.label)
 		else:
 			title = item.parent().text(0)
 			index = item.parent().indexOfChild(item)
-			print(title)
-			print(item.data(0, QtCore.Qt.UserRole))
+			curve = item.data(0, QtCore.Qt.UserRole)
+			if (curve.line):
+				if (item.checkState(0) == 0): 
+					curve.line.set_visible(False)
+					curve.line.set_label('_nolegend_')
+				else: 
+					curve.line.set_visible(True)
+					curve.line.set_label(curve.label)
+		self.canvas._replot()
 
-			# self._switchCheckStatusWithFigure(title, index, item.checkState(0), item.text(0))
-		# self._replot()
-
-
+	
 
 # Clear data
 	def clearData(self):
